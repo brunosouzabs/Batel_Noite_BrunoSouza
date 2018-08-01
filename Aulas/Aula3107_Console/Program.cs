2eef0e025d70b8669c064e219f0b7568f18ab273
brunosouzabs <bsbdso@gmail.com>
2018-07-31 21:55:00 -0300

    Calculadora gera operações na tela

--- Aulas/Aula3107_Console/Program.cs
+++ Aulas/Aula3107_Console/Program.cs
@@ -8,24 +8,30 @@ namespace Aula3107_Console
 {
     class Program
     {
+        public static int nun1 { get; set; }
+        public static int nun2 { get; set; }
+        public static int op { get; set; }
         private int id;
-
-        public static int Id { get; set; }
-       
-
         static void Main(string[] args)
         {
             
-            Console.WriteLine("Fucking World!!");
+        Console.WriteLine("Digite o Primeiro Número:");
+            nun1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o Segundo Número:");
+            nun2 = int.Parse(Console.ReadLine());
+
             
 
-            Console.WriteLine("Digite um número:");
-            Id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Em Soma: " + (nun1+nun2));
+            Console.WriteLine("Em Subtração: " + (nun1 - nun2));
+            Console.WriteLine("Em Divisão: " + (nun1 / nun2));
+            Console.WriteLine("Em Multiplicação: " +(nun1 * nun2));
 
-            Console.WriteLine("Você digitou: " + Id);
             Console.ReadKey();           
 
-
+            
         }
+        
     }
 }
