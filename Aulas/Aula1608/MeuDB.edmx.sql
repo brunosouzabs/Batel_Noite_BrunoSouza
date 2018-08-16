abe8edef9941c481ff9f80617f6139b36041208e
Bruno Souza <bsbdso@gmail.com>
2018-08-16 20:12:47 -0300

    com db

---
+++ Aulas/Aula1608/MeuDB.edmx.sql
@@ -0,0 +1,88 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 08/16/2018 20:11:53
+-- Generated from EDMX file: C:\Users\1611972\Source\Repos\Batel_Noite_BrunoSouza\Aulas\Aula1608\MeuDB.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [PrimeiraDB];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'Clientes'
+CREATE TABLE [dbo].[Clientes] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Nome] nvarchar(max)  NOT NULL,
+    [CPF] nvarchar(max)  NOT NULL,
+    [Ativo] bit  NOT NULL,
+    [EnderecoId] int  NOT NULL
+);
+GO
+
+-- Creating table 'Enderecos'
+CREATE TABLE [dbo].[Enderecos] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Logradourou] nvarchar(max)  NOT NULL,
+    [Nome] nvarchar(max)  NOT NULL,
+    [Numero] nvarchar(max)  NOT NULL,
+    [CEP] nvarchar(max)  NOT NULL,
+    [ClienteId] int  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'Clientes'
+ALTER TABLE [dbo].[Clientes]
+ADD CONSTRAINT [PK_Clientes]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'Enderecos'
+ALTER TABLE [dbo].[Enderecos]
+ADD CONSTRAINT [PK_Enderecos]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [ClienteId] in table 'Enderecos'
+ALTER TABLE [dbo].[Enderecos]
+ADD CONSTRAINT [FK_ClienteEndereco]
+    FOREIGN KEY ([ClienteId])
+    REFERENCES [dbo].[Clientes]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_ClienteEndereco'
+CREATE INDEX [IX_FK_ClienteEndereco]
+ON [dbo].[Enderecos]
+    ([ClienteId]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
