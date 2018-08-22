cd32e80323fc2d4f920a28258864fae674c9012b
brunosouzabs <bsbdso@gmail.com>
2018-08-21 21:32:05 -0300

    Escrevendo estrutura da classa categoria (Modelo)

---
+++ Aulas/Aula2108_CodeFirst/Modelos/Categosrias.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula2108_CodeFirst.Modelos
+{
+    class Categosrias
+    {
+        [Key]
+        public int CategoriaID { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+    }
+}
