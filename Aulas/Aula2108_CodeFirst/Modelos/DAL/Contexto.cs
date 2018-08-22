f61d195941bd84db157d13e01c831e3628dc3f16
brunosouzabs <bsbdso@gmail.com>
2018-08-21 21:59:47 -0300

    Herdaando a DBcontext

--- Aulas/Aula2108_CodeFirst/Modelos/DAL/Contexto.cs
+++ Aulas/Aula2108_CodeFirst/Modelos/DAL/Contexto.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Aula2108_CodeFirst.Modelos.DAL
 {
-    public class Contexto
+    //Deve herdar da DBcontext
+    public class Contexto : DbContext
     {
+        public Contexto() : base("nomeStringConexao")
+        {
+
+        }
+
 
     }
 }
